b2117e9ff6341c4eee3152a6bee651702d7384bd
Heru Handika <herubiolog@gmail.com>
2022-08-03 11:40:00 -0500

    Fix test issues.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,24 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:trapir/main.dart';
+import 'package:trapir/screens/projects/new_project.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('Test email is valid', () {
+    String email = 'test@gmail.com';
+    String email2 = 'test\$\$#%@gmail';
+    expect(email.isValidEmail, isTrue);
+    expect(email2.isValidEmail, isFalse);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('Test validation for project names', () {
+    String projectName = 'Project Name';
+    String projectName2 = 'Project Name2';
+    String projectName3 = 'Project Name!?*';
+    expect(projectName.isValidProjectName, isTrue);
+    expect(projectName2.isValidProjectName, isTrue);
+    expect(projectName3.isValidProjectName, isFalse);
   });
 }
